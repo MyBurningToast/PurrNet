1ba3e99c3c4c1163d3bbf10f45d70483e4b217e5
Max Martin <treblaxam@gmail.com>
2026-09-21 17:02:21 +1200

    Only override sender with player when receiving as server

--- Assets/PurrNet/Runtime/CoreModules/RPCs/RPCModule.cs
+++ Assets/PurrNet/Runtime/CoreModules/RPCs/RPCModule.cs
@@ -934,7 +934,7 @@ namespace PurrNet.Modules
             var info = new RPCInfo
             {
                 manager = _manager,
-                sender = player,
+                sender = asServer ? player : data.header.senderId,
                 asServer = asServer,
                 receivedImmediate = _receivingImmediateLane
             };
@@ -968,7 +968,7 @@ namespace PurrNet.Modules
             var info = new RPCInfo
             {
                 manager = _manager,
-                sender = player,
+                sender = asServer ? player : packet.header.senderId,
                 asServer = asServer,
                 receivedImmediate = _receivingImmediateLane
             };
@@ -1091,7 +1091,7 @@ namespace PurrNet.Modules
             var info = new RPCInfo
             {
                 manager = _manager,
-                sender = player,
+                sender = asServer ? player : packet.header.senderId,
                 asServer = asServer,
                 receivedImmediate = _receivingImmediateLane
             };
